2a0c079a53e78f94a4ad2aaa7c6f0ba8285f36c3
jean Sébastien <webmanhtc@gmail.com>
2025-05-19 03:02:11 +0200

    mise en place de 4 from de 10 question

---
+++ prisma/migrations/20250518162719_init/migration.sql
@@ -0,0 +1,139 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Quizz" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Quizz_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" SERIAL NOT NULL,
+    "timeMax" INTEGER NOT NULL,
+    "scoreMax" INTEGER NOT NULL,
+    "scoreMin" INTEGER NOT NULL,
+    "question" TEXT NOT NULL,
+    "explication" TEXT NOT NULL,
+    "quizzId" INTEGER NOT NULL,
+
+    CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Answer" (
+    "id" SERIAL NOT NULL,
+    "isGoodAnswers" BOOLEAN NOT NULL,
+    "name" TEXT NOT NULL,
+    "questionId" INTEGER NOT NULL,
+
+    CONSTRAINT "Answer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SessionQuizz" (
+    "id" SERIAL NOT NULL,
+    "quizzId" INTEGER NOT NULL,
+    "presenterId" INTEGER NOT NULL,
+    "startAt" TIMESTAMP(3) NOT NULL,
+    "endAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "SessionQuizz_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserResponse" (
+    "id" SERIAL NOT NULL,
+    "questionId" INTEGER NOT NULL,
+    "answerId" INTEGER NOT NULL,
+    "sessionQuizzId" INTEGER NOT NULL,
+    "score" INTEGER NOT NULL,
+    "responseTime" DOUBLE PRECISION NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "UserResponse_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_sessionParticipants" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_sessionParticipants_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserResponse_questionId_key" ON "UserResponse"("questionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserResponse_sessionQuizzId_key" ON "UserResponse"("sessionQuizzId");
+
+-- CreateIndex
+CREATE INDEX "_sessionParticipants_B_index" ON "_sessionParticipants"("B");
+
+-- AddForeignKey
+ALTER TABLE "Quizz" ADD CONSTRAINT "Quizz_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Quizz" ADD CONSTRAINT "Quizz_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Question" ADD CONSTRAINT "Question_quizzId_fkey" FOREIGN KEY ("quizzId") REFERENCES "Quizz"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Answer" ADD CONSTRAINT "Answer_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SessionQuizz" ADD CONSTRAINT "SessionQuizz_quizzId_fkey" FOREIGN KEY ("quizzId") REFERENCES "Quizz"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SessionQuizz" ADD CONSTRAINT "SessionQuizz_presenterId_fkey" FOREIGN KEY ("presenterId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserResponse" ADD CONSTRAINT "UserResponse_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserResponse" ADD CONSTRAINT "UserResponse_answerId_fkey" FOREIGN KEY ("answerId") REFERENCES "Answer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserResponse" ADD CONSTRAINT "UserResponse_sessionQuizzId_fkey" FOREIGN KEY ("sessionQuizzId") REFERENCES "SessionQuizz"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserResponse" ADD CONSTRAINT "UserResponse_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_sessionParticipants" ADD CONSTRAINT "_sessionParticipants_A_fkey" FOREIGN KEY ("A") REFERENCES "SessionQuizz"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_sessionParticipants" ADD CONSTRAINT "_sessionParticipants_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
